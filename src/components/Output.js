f2c07a4588162fded4ef609119e48f3193926381
Silvia Phungky <silvia@tinkerlust.com>
2019-10-25 14:42:03 +0700

    calculator output screen

---
+++ src/components/Output.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Form } from 'react-bootstrap'
+
+const Output = ({result}) => {
+  return(
+    <div className='output'>
+      {result}
+    </div>
+
+  )
+}
+
+export default Output
